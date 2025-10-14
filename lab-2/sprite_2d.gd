ccd4016683bd939ea1d2f9334353170ca7adcc22
emmanuelokeke <eokeke@fordham.edu>
2025-10-14 14:16:45 -0400

    LAB 2 DONE

--- lab-2/sprite_2d.gd
+++ lab-2/sprite_2d.gd
@@ -1,1 +1,15 @@
 extends Sprite2D
+
+@export var health = 10
+@export var booleanVar = true
+
+@export var speed = 5.
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+#called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("move_right"):
+		position.x += speed
+		print("haha")
